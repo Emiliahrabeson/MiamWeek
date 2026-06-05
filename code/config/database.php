2f0d4d5a8349ee421f0ba4293b8bab67bba7d5a2
Emiliahrabeson <emiliahrabeson@gmail.com>
2026-06-05 10:00:44 +0300

    feat(*): MVC Architecture

--- code/config/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'host' => 'localhost',
-    'dbname' => 'Nutrition',
-    'user' => 'emiliah_nutrition',
-    'password' => 'Emi1234@'
-];
--- code/public/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/../app/core/Router.php';
-session_start();
-
-Router::route();
